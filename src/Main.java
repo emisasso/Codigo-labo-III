f1c2d48faedd698b54e56ad522a679d558648a50
Emiliano sasso <emi_sasso@hotmail.com>
2023-03-28 01:15:59 -0300

    segundo commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("La concha de tu vieja git y la puta que te pario");
     }
 }
