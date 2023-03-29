832d843f320372e3423524907039654954a7446a
emisasso <129129960+emisasso@users.noreply.github.com>
2023-03-28 22:17:11 -0300

    Delete src directory

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("La concha de tu vieja git y la puta que te pario");
-    }
-}
